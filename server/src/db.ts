3c133026609f75d24fd92b9db0e6983a893e1836
JosePuelloMarval <jpuello@marval.com.co>
2025-01-31 14:39:33 -0500

    agregando las variables de entorno a la configuracion de la base de datos

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,13 +1,18 @@
 import { DataSource } from "typeorm";
+import  "dotenv/config";
 import { User } from "./entities/users/User";
+
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE} =  process.env;
+console.log(DB_DATABASE, DB_HOST);
+
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "admin123",
-    database: "dashboard", 
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE, 
     synchronize: true,
     logging: false,
     entities: [
