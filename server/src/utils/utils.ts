a047f5c703ddb2bf8e34f71a8e8196cfee225e5a
RobinsonMosqueraMarval <197088806+RobinsonMosqueraMarval@users.noreply.github.com>
2025-02-03 15:41:20 -0500

    feat: :sparkles: Funcion validar telefono

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -2,3 +2,7 @@ export const validatorEmail = (email: string): boolean => {
     const pattern = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,10}$/;
     return pattern.test(email);
 };
+export const validaTel = (tel: string): boolean => {
+    const pattern = /^\+?\d{1,3}?[-.\s]?\(?\d{2,4}\)?[-.\s]?\d{3,4}[-.\s]?\d{4}$/;
+    return pattern.test(tel);
+};
